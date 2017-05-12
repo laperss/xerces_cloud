d7e2867c37b57dcd3b88ea457b8f9b5d69ee284a
quilder <quilder@users.noreply.github.com>
2017-05-12 14:53:25 +0200

    Add functionality to automatically update /etc/hosts

--- vm-init.sh
+++ vm-init.sh
@@ -3,3 +3,14 @@
 # Update packages
 sudo apt-get -y update
 sudo apt-get -y upgrade
+
+# Add ericsson to /etc/hosts
+IP="129.192.68.4"
+HOSTNAME="xerces.ericsson.net"
+
+if [ -n "$(grep $HOSTNAME /etc/hosts)" ]
+then
+        echo "$HOSTNAME already added in /etc/hosts"
+else
+        sed -i "2i$IP $HOSTNAME" /etc/hosts
+fi
